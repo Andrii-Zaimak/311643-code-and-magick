9bb22d94c0788423d2addaa69ec7f7dc12294915
Andrii-Zaimak <andreyzaymak@gmail.com>
2017-01-23 09:25:42 +0200

    Исправлены замечания

--- js/stat.js
+++ js/stat.js
@@ -5,62 +5,66 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, stats) {
-  var windowWidth = 420;
-  var windowHeight = 270;
-  var windowX = 100;
-  var windowY = 10;
-  var shadowOffset = 10;
+  var WINDOW_WIDTH = 420;
+  var WINDOW_HEIGHT = 270;
+  var WINDOW_X = 100;
+  var WINDOW_Y = 10;
+  var WINDOW_SHADOW_OFFSET = 10;
+  var i;
 
   // window shadow
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(windowX + shadowOffset, windowY + shadowOffset, windowWidth, windowHeight);
+  ctx.fillRect(WINDOW_X + WINDOW_SHADOW_OFFSET, WINDOW_Y + WINDOW_SHADOW_OFFSET, WINDOW_WIDTH, WINDOW_HEIGHT);
   // window fill
-  ctx.fillStyle = 'rgba(255, 255, 255, 1.0)';
-  ctx.strokeRect(windowX, windowY, windowWidth, windowHeight);
-  ctx.fillRect(windowX, windowY, windowWidth, windowHeight);
+  ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+  ctx.strokeRect(WINDOW_X, WINDOW_Y, WINDOW_WIDTH, WINDOW_HEIGHT);
+  ctx.fillRect(WINDOW_X, WINDOW_Y, WINDOW_WIDTH, WINDOW_HEIGHT);
 
   // window won message
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
 
-  var textY = 40;
-  ctx.fillText('Ура вы победили!', 120, textY);
-  ctx.fillText('Список результатов:', 120, textY + 20);
+  ctx.fillText('Ура вы победили!', WINDOW_X + 20, WINDOW_Y + 40);
+  ctx.fillText('Список результатов:', WINDOW_X + 20, WINDOW_Y + 60);
 
   // chart
-  var maxPlayerTime = parseInt(stats[0], 10);
-  var i = 0;
+  var maxPlayerTime;
 
   for (i = 0; i < stats.length; i++) {
+    // convert to int
     stats[i] = parseInt(stats[i], 10);
 
+    // find max value
     if (i === 0 || stats[i] > maxPlayerTime) {
       maxPlayerTime = stats[i];
     }
   }
+  // draw chart items
+  for (i = 0; i < stats.length; i++) {
+    drawChartItem(names[i], stats[i], i);
+  }
 
-  var drawChartItem = function (name, stat, index) {
+  function drawChartItem(name, stat, index) {
+    var START_X = WINDOW_X + 60;
+    var MAX_HEIGHT = 150;
     var myName = 'Вы';
     var offset = 50;
     var width = 40;
-    var height = parseInt(150 * stat / maxPlayerTime, 10);
-    var x = 160 + (width + offset) * index;
-    var y = windowY + windowHeight - height - 30;
+    var height = parseInt(MAX_HEIGHT * stat / maxPlayerTime, 10);
+    var x = START_X + (width + offset) * index;
+    var y = WINDOW_Y + WINDOW_HEIGHT - height - 30;
 
     // chart
-    ctx.fillStyle = name === myName ? 'rgba(255, 0, 0, 1.0)' : 'rgba(0, 0, ' + ((Math.random() * 5) * 50).toFixed(0) + ', 1.0)';
+    ctx.fillStyle = name === myName ? 'rgba(255, 0, 0, 1)' : 'rgba(0, 0, ' + ((Math.random() * 5) * 50).toFixed(0) + ', 1.0)';
     ctx.fillRect(x, y, width, height);
 
     // name
     ctx.fillStyle = '#000';
     ctx.font = '16px PT Mono';
 
-    ctx.fillText(name, x, windowY + windowHeight - 10);
+    ctx.fillText(name, x, WINDOW_Y + WINDOW_HEIGHT - 10);
 
     // time
     ctx.fillText(stat, x, y - 5);
-  };
-  for (i = 0; i < stats.length; i++) {
-    drawChartItem(names[i], stats[i], i);
   }
 };
