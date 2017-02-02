41da8ac3dde41d899e8d656dce49379c3ccb3644
Andrii-Zaimak <andreyzaymak@gmail.com>
2017-02-02 15:37:40 +0200

    Добавлены атрибуты ARIA, поддержка кнопок ENTER/ESCAPE в диалоговом окне

--- js/setup.js
+++ js/setup.js
@@ -3,6 +3,9 @@
  */
 'use strict';
 
+var KEY_CODE_ENTER = 13;
+var KEY_CODE_ESCAPE = 27;
+
 var wizardCoatColorsList = [
   'rgb(101, 137, 164)',
   'rgb(241, 43, 107)',
@@ -29,6 +32,7 @@ var wizardFireballColorsList = [
 var setupWindowNode = document.querySelector('.setup');
 var setupOpenBtnNode = document.querySelector('.setup-open');
 var setupCloseBtnNode = setupWindowNode.querySelector('.setup-close');
+var setupSaveBtnNode = setupWindowNode.querySelector('.setup-submit');
 var userNameNode = setupWindowNode.querySelector('.setup-user-name');
 var wizardCoatNode = setupWindowNode.querySelector('#wizard-coat');
 var wizardEyesNode = setupWindowNode.querySelector('#wizard-eyes');
@@ -37,6 +41,8 @@ var wizardFireballNode = setupWindowNode.querySelector('.setup-fireball-wrap');
 var wizardCoatIndex = 0;
 var wizardEyesIndex = 0;
 var wizardFireballIndex = 0;
+
+var lastFocusElement;
 
 // add validation option to user name field
 // - no empty
@@ -60,11 +66,80 @@ wizardFireballNode.addEventListener('click', function () {
 });
 
 // open setup window
-setupOpenBtnNode.addEventListener('click', function () {
-  setupWindowNode.classList.remove('invisible');
+setupOpenBtnNode.addEventListener('click', function (evt) {
+  openSetupWindow(evt);
+});
+
+setupOpenBtnNode.addEventListener('keydown', function (evt) {
+  if (isValidKeyPressed(evt.keyCode, KEY_CODE_ENTER)) {
+    openSetupWindow(evt);
+  }
 });
 
 // close setup window
 setupCloseBtnNode.addEventListener('click', function () {
-  setupWindowNode.classList.add('invisible');
+  closeSetupWindow();
 });
+
+setupCloseBtnNode.addEventListener('keydown', function (evt) {
+  if (isValidKeyPressed(evt.keyCode, KEY_CODE_ENTER)) {
+    closeSetupWindow();
+  }
+});
+
+// save setup settings
+setupSaveBtnNode.addEventListener('click', function () {
+  closeSetupWindow();
+});
+
+setupSaveBtnNode.addEventListener('keydown', function (evt) {
+  if (isValidKeyPressed(evt.keyCode, KEY_CODE_ENTER)) {
+    closeSetupWindow();
+  }
+});
+
+// open dialog
+function openSetupWindow(evt) {
+  setupWindowNode.classList.remove('invisible');
+
+  document.addEventListener('keydown', keyDownHandler);
+  document.addEventListener('focus', changeFocusHandler, true);
+
+  // save last focused element
+  lastFocusElement = evt.target;
+  // set focus to setup window
+  setupWindowNode.focus();
+}
+// close dialog
+function closeSetupWindow() {
+  setupWindowNode.classList.add('invisible');
+
+  // return focus to last focused element before open setup window
+  if (lastFocusElement) {
+    lastFocusElement.focus();
+    lastFocusElement = null;
+  }
+
+  // remove unused listeners
+  document.removeEventListener('keydown', keyDownHandler);
+  document.removeEventListener('focus', changeFocusHandler, true);
+}
+
+function keyDownHandler(evt) {
+  if (isValidKeyPressed(evt.keyCode, KEY_CODE_ESCAPE)) {
+    closeSetupWindow();
+  }
+}
+
+// check for need key is pressed
+function isValidKeyPressed(keyCode, needKey) {
+  return keyCode && keyCode === needKey;
+}
+
+// change focus in dialog
+function changeFocusHandler(evt) {
+  if (!setupWindowNode.contains(evt.target)) {
+    evt.preventDefault();
+    setupCloseBtnNode.focus();
+  }
+}
